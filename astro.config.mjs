96765cb5a196b6fa4429de80dbb546f773737f2a
Hengwei Li <141338337+leevelyn@users.noreply.github.com>
2026-09-04 17:33:15 +0800

    Set site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
+  site: "https://leevelyn.github.io",
   vite: {
     plugins: [tailwindcss()],
   },
